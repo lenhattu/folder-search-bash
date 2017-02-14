fa31f078c2d1e87a3de6460e84cfbe25d37462c0
lenhattu <lenhattu12@gmail.com>
2017-02-13 17:09:03 -0800

    added user choice

--- simsearch.sh
+++ simsearch.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+#init array
 array=()
+
+#find folders and fill results in array
 while read -r folder; do
     array=("${array[@]}" "$folder")
 done < <(find /Users/tule/Dropbox/Research/Bioinformatics/Practice/targetA -type d -name "sim*") 
 
+#display array
 for i in "${!array[@]}"; do
     printf "%s %s\n" "$i" "${array[$i]}"
 done
 
+#user choice
+read -p "Enter the number: " choice
+echo ${array[choice]}
